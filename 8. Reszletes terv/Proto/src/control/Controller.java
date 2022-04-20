ec3fb1c8733ee5fde24e018df8450e0d2fa901e7
dancsomarci <dancsomarci@gmail.com>
2022-04-20 12:14:07 +0200

    TODO update

--- 8. Reszletes terv/Proto/src/control/Controller.java
+++ 8. Reszletes terv/Proto/src/control/Controller.java
@@ -4,10 +4,11 @@ package control;
 //TODO-------------------------------
 //TODO 1) Virologist::Move() -nál most a mező default neighbourjára lép, aki a currentField parancsnál
 //        a Neighbour: listában az első helyen áll!, ez nincs dokumentálva sehol, ha jól emlékszem!
-//          - Vencel: szeintem beleírtam a bementi nyelv leírásánál, hogy a 0.szomszédra megy, de ez det. eset csak
+//          -Vencel: szeintem beleírtam a bementi nyelv leírásánál, hogy a 0.szomszédra megy, de ez det. eset csak
+//          -Marci: Én nem találom, de lehet csak vak vagyok.
 //TODO 2) +komplex tesztesetek amit kért Goldi (vagy 9 azt hiszem) Vencel:
-//          Vencel: nem 3, amin 3 jatekos van es 3at lepnek legalabb? Vagy hogy volt ez?
-//          Marci: Én 9-re emlékszem, de igazából mind1 kap 9 olyat, ami után megnyalja mind a 10 ujját!
+//          -Vencel: nem 3, amin 3 jatekos van es 3at lepnek legalabb? Vagy hogy volt ez?
+//          -Marci: Én 9-re emlékszem, de igazából mind1 kap 9 olyat, ami után megnyalja mind a 10 ujját!
 
 
 //TODO DONE + változtatások lényege
