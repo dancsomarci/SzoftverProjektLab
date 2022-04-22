02226c70fa7db144e70079aa9bf37dcd7291c9a7
Breinich <bajnokvencel@edu.bme.hu>
2022-04-22 02:06:47 +0200

    GeneticCode comments

--- 8. Reszletes terv/Proto/src/model/codes/GeneticCode.java
+++ 8. Reszletes terv/Proto/src/model/codes/GeneticCode.java
@@ -13,30 +13,44 @@ import model.agents.Agent;
  */
 public abstract class GeneticCode
 {
+	/**
+	 * Object equals függvényének felülírása 2 genetikai kód összehasonlítására.
+	 * @param o az összehasonlítandó objektum.
+	 * @return megegyezik-e a két objektum.
+	 */
 	@Override
 	public boolean equals(Object o){
 		return this.getClass().getSimpleName().equals(o.getClass().getSimpleName());
 	}
 
+	/**
+	 * @return a genetikai kód típusa.
+	 */
 	public String getName(){
 		return this.getClass().getSimpleName();
 	}
 
+	/**
+	 * @return a genetikai kód által készíthető ágens aminosav költsége.
+	 */
 	public int getAminoAcidPrice() {
 		return aminoAcidPrice;
 	}
 
+	/**
+	 * @return a genetikai kód által készíthető ágens nukleotid költsége.
+	 */
 	public int getNucleotidePrice() {
 		return nucleotidePrice;
 	}
 
 	/**
-	 * A kódhoz tartozó ágens előállításához szükséges aminosav mennyiség
+	 * A kódhoz tartozó ágens előállításához szükséges aminosav mennyiség.
 	 */
 	protected int aminoAcidPrice;
 
 	/**
-	 * A kódhoz tartozó ágens előállításához szükséges nukleotid mennyiség
+	 * A kódhoz tartozó ágens előállításához szükséges nukleotid mennyiség.
 	 */
 	protected int nucleotidePrice;
 
