a5b6492f9199a4faa3e6ef75bb900ba7a82ca88c
Breinich <bajnokvencel@edu.bme.hu>
2022-04-22 02:14:54 +0200

    Agent comments

--- 8. Reszletes terv/Proto/src/model/agents/Agent.java
+++ 8. Reszletes terv/Proto/src/model/agents/Agent.java
@@ -10,27 +10,45 @@ import model.Virologist;
  */
 public abstract class Agent
 {
+	/**
+	 * Az Object equals függvényének felülírása, összehasonlítja ezt az objektumot a paraméterül kapottal (mely jellemzően egy ágens).
+	 * @param o az összehasonlítandó objektum.
+	 * @return megegyezik-e a 2 objektum típusa.
+	 */
 	@Override
 	public boolean equals(Object o){
 		return this.getClass().getSimpleName().equals(o.getClass().getSimpleName());
 	}
 
+	/**
+	 * @return az ágens típusa.
+	 */
 	public String getName(){
 		return this.getClass().getSimpleName();
 	}
 
+	/**
+	 * @return az ágens hátralévő hatásideje szöveg típusként.
+	 */
 	public String getTimeToLive(){return String.valueOf(timeToLive);}
 
+	/**
+ 	 * @return az ágens hátralévő hatásideje.
+	 */
 	public int getTtl(){
 		return timeToLive;
 	}
 
+	/**
+	 * Beállítja az ágens hatásidejét.
+	 * @param ttl a beállítandó élettartam.
+	 */
 	public void setTtl(int ttl){
 		timeToLive = ttl;
 	}
 
 	/**
-	 * Az ágens hátra lévő élettartama
+	 * Az ágens hátra lévő élettartama.
 	 */
 	protected int timeToLive;
 
@@ -41,7 +59,7 @@ public abstract class Agent
 	/**
 	 * Öregíti egy egységgel az Agent-et.
 	 * Ha lejárt eltávolítja magát a virológusról, és reseteli azt.
-	 * @param v a tulajdonos virológus
+	 * @param v a tulajdonos virológus.
 	 */
 	public void Update(Virologist v)
 	{
@@ -55,7 +73,7 @@ public abstract class Agent
 	/**
 	 * Default implementációban üres függvény, de azt reprezentálja, hogy az ágenst elhelyezik a paraméterül kapott virológuson
 	 * és esetlegesen ennek vannak direkt/azonnali hatásai rá.
-	 * @param v a célzott virológus
+	 * @param v a célzott virológus.
 	 */
 	public void Apply(Virologist v)
 	{
@@ -65,7 +83,7 @@ public abstract class Agent
 	 * Az ágens hatásait hajtja végre a virológuson,
 	 * átállítja bizonyos cselekvéseinek a működését,
 	 * alapértelmezetten üres, amikor nincs az ágensnek hosszútávú hatása, csak azonnali.
-	 * @param v a célzott virológus
+	 * @param v a célzott virológus.
 	 */
 	public void ApplyStrategy(Virologist v)
 	{
