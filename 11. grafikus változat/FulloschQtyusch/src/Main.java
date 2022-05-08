2f9a2f2792f2f42258ae765afb5eeecd14dc87d3
dancsomarci <dancsomarci@gmail.com>
2022-05-08 14:16:08 +0200

    controller class update

--- 11. grafikus változat/FulloschQtyusch/src/Main.java
+++ 11. grafikus változat/FulloschQtyusch/src/Main.java
@@ -1,5 +1,13 @@
+import control.Controller;
+import control.Loader;
+
 public class Main {
     public static void main(String[] args){
-        
+        try {
+            Loader loader = new Loader();
+            Controller controller = new Controller(loader.loadGame(args[0]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- 11. grafikus változat/FulloschQtyusch/src/control/Controller.java
+++ 11. grafikus változat/FulloschQtyusch/src/control/Controller.java
@@ -1,5 +1,61 @@
 package control;
 
-public class Controller {
+import model.Game;
+import model.Subject;
+import model.Virologist;
+import model.codes.GeneticCode;
+import model.map.Field;
+import view.Window;
 
+public class Controller extends Subject{
+    private String actionMessage;
+    private Game game;
+    private Window window;
+
+    public Controller(Game game) {
+        this.game = game;
+        this.actionMessage = "";
+        window = new Window(this, game);
+    }
+
+    public void attack(Virologist v) {
+    }
+
+    public void move(Field f) {
+    }
+
+    public void drop() {
+    }
+
+    public void lootAminoFrom(Virologist v) {
+    }
+
+    public void lootNucleoFrom(Virologist v) {
+    }
+
+    public void lootEquipmentFrom(Virologist v) {
+    }
+
+    public void collect() {
+    }
+
+    public void learn() {
+    }
+
+    public void equip() {
+    }
+
+    public void inject(Virologist v, GeneticCode code) {
+    }
+
+    public void endTurn() {
+    }
+
+    public String getActionMessage() {
+        return actionMessage;
+    }
+
+    public void update() {
+    }
 }
+
