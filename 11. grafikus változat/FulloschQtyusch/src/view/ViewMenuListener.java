a8300db6a22ec54852e9157d7d904b06c02fb6be
Andras-T <80324180+Andras-T@users.noreply.github.com>
2022-05-08 16:33:42 +0200

    Window Alpha 0.1

---
+++ 11. grafikus változat/FulloschQtyusch/src/view/ViewMenuListener.java
@@ -0,0 +1,26 @@
+package view;
+
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class ViewMenuListener implements MenuListener {
+
+    Runnable action;
+
+    public ViewMenuListener(Runnable action){
+        this.action = action;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+        action.run();
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+    }
+}
--- 11. grafikus változat/FulloschQtyusch/src/view/Window.java
+++ 11. grafikus változat/FulloschQtyusch/src/view/Window.java
@@ -2,10 +2,23 @@ package view;
 
 import control.Controller;
 import model.Game;
+import model.Virologist;
+import model.codes.GeneticCode;
+import model.map.Field;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.border.Border;
 
-public class Window extends Observer {
+public class Window extends Observer{
+
+
 
     private JMenuBar menuBar;
     private JButton endButton;
@@ -26,17 +39,137 @@ public class Window extends Observer {
         mainMenu.add(actions);
         f.setJMenuBar(mainMenu);
 
+        JMenu attack= new JMenu("attack");
+        actions.add(attack);
+        attack.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Virologist vir : v.getField().GetVirologists() ) {
+//                JMenuItem item = new JMenuItem(vir.getName());
+//                item.addActionListener((e)->controller.attack(vir));
+//                attack.add(item);
+//            }
+        }));
 
+        JMenu move= new JMenu("move");
+        actions.add(move);
+        move.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Field field : v.getField().GetNeighbours() ) {
+//                JMenuItem item = new JMenuItem(field.getName());
+//                item.addActionListener((e)->controller.move(field));
+//                attack.add(item);
+//            }
+        }));
 
-//        JMenu subMenu= new JMenu("items");
-//        JMenuItem i1=new JMenuItem("Item 1");
-//        JMenuItem i2=new JMenuItem("Item 2");
-//        JMenuItem i3=new JMenuItem("Item 3");
-//        JMenuItem i4=new JMenuItem("Item 4");
-//        JMenuItem i5=new JMenuItem("Item 5");
+        JMenuItem drop = new JMenuItem("drop");
+        actions.add(drop);
+        drop.addActionListener((e)->controller.drop());
 
+        JMenu lootAminoFrom = new JMenu("lootAminoFrom");
+        actions.add(lootAminoFrom);
+        lootAminoFrom.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Virologist vir : v.getField().GetVirologists() ) {
+//                if (!vir.equals(v)){
+//                    JMenuItem item = new JMenuItem(vir.getName());
+//                    item.addActionListener((e)->controller.lootAminoFrom(vir));
+//                    attack.add(item);
+//                }
+//            }
+        }));
 
+        JMenu lootNucleoFrom = new JMenu("lootNucleoFrom");
+        actions.add(lootNucleoFrom);
+        lootNucleoFrom.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Virologist vir : v.getField().GetVirologists() ) {
+//                if (!vir.equals(v)){
+//                    JMenuItem item = new JMenuItem(vir.getName());
+//                    item.addActionListener((e)->controller.lootNucleoFrom(vir));
+//                    attack.add(item);
+//                }
+//            }
+        }));
+
+        JMenu lootEquipmentFrom = new JMenu("lootEquipmentFrom");
+        actions.add(lootEquipmentFrom);
+        lootEquipmentFrom.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Virologist vir : v.getField().GetVirologists() ) {
+//                if (!vir.equals(v)){
+//                    JMenuItem item = new JMenuItem(vir.getName());
+//                    item.addActionListener((e)->controller.lootEquipmentFrom(vir));
+//                    attack.add(item);
+//                }
+//            }
+        }));
+
+        JMenuItem collect=new JMenuItem("collect");
+        actions.add(collect);
+        collect.addActionListener((e)->controller.collect());
+
+        JMenuItem learn=new JMenuItem("learn");
+        actions.add(learn);
+        learn.addActionListener((e)->controller.learn());
+
+        JMenuItem equip=new JMenuItem("equip");
+        actions.add(equip);
+        equip.addActionListener((e)->controller.equip());
+
+        JMenu inject = new JMenu("inject");
+        actions.add(inject);
+        inject.addMenuListener(new ViewMenuListener(()->{
+//            Virologist v = game.GetCurrentPlayer();
+//            for (Virologist vir : v.getField().GetVirologists() ) {
+//                JMenu virMenu = new JMenu(vir.getName());
+//                virMenu.addMenuListener(new ViewMenuListener(()->{
+//                    for (GeneticCode code : v.getGeneticCodes()){
+//                        JMenuItem codeItem = new JMenuItem(code.getName());
+//                        codeItem.addActionListener((e)->controller.inject(vir, code));
+//                        virMenu.add(codeItem);
+//                    }
+//                }));
+//                attack.add(virMenu);
+//            }
+        }));
+
+        JMenuItem endTurn=new JMenuItem("endTurn");
+        actions.add(endTurn);
+        endTurn.addActionListener((e) -> controller.endTurn());
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 600, 600);
+
+        /*
+    private JProgressBar aminoBar;
+    private JProgressBar nucleoBar;
+    private JLabel aminoLabel;
+    private JLabel nucleoLabel;
+    private JLabel turnCounter;
+    private JLabel actionBubble;
+         */
+        Image endButtonIcon = null;
+        try {
+            endButtonIcon = ImageIO.read(new File("textures/endButton.png"));
+            endButtonIcon = endButtonIcon.getScaledInstance(70, 70, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        endButton = new JButton(new ImageIcon(endButtonIcon));
+        endButton.setContentAreaFilled(true);
+        endButton.setBorder(null);
+        endButton.setOpaque(false);
+        endButton.setBorderPainted(false);
+        endButton.setBackground(Color.GREEN);
+        endButton.setBounds(450, 450, 70, 70);
+        endButton.setBorder(new RoundedBorder(35));
+
+        aminoBar = new JProgressBar();
+
+        layeredPane.add(endButton, Integer.valueOf(0));
+        f.add(layeredPane);
         f.setSize(600,600);
+        f.setResizable(false);
         f.setLayout(null);
         f.setVisible(true);
     }
@@ -53,6 +186,31 @@ public class Window extends Observer {
     @Override
     public void update(){
 
+    }
+
+    private static class RoundedBorder implements Border {
+
+        private int radius;
+
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+        }
     }
 
 }
